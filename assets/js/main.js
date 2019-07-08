2907c2676359cc05bcdf1adfbf4e7a3a8a2f3316
glaydsonp <38405677+glaydsonp@users.noreply.github.com>
2019-07-07 22:28:25 -0300

    donate page done

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,19 @@ $(".zoomImage").mouseout(function () {
     $(this).css("transform", "scale(1.0)");
     $(this).css("transition", "0.2s");
 });
+$("#pacoteUm").click(function () {
+    var url = "https://www.mercadopago.com.br/checkout/v1/redirect?preference-id=374288851-4479eb71-c64b-4f5d-8a0b-16c71b18fc12";
+    document.location.href = url;
+});
+$("#pacoteDois").click(function () {
+    var url2 = "https://www.mercadopago.com.br/checkout/v1/redirect?preference-id=374288851-86106f5a-9a49-4b11-8596-411e6e048366";
+    document.location.href = url2;
+});
+$("#pacoteTres").click(function () {
+    var url3 = "https://www.mercadopago.com.br/checkout/v1/redirect?preference-id=374288851-fdf8897e-939c-4460-9fe7-854ccdbdda0e";
+    document.location.href = url3;
+});
+$("#pacoteQuatro").click(function () {
+    var url4 = "https://www.mercadopago.com.br/checkout/v1/redirect?preference-id=374288851-608c9f75-5e41-42cb-bdf1-bad0e80127cb";
+    document.location.href = url4;
+});
